fd9154def6d71d5be74f277fd0ef7347a62793a2
donfsucha <cha1188@gmail.com>
2026-01-15 11:39:06 +0900

    feat: Adjust Num Range Again

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,11 @@
-vi fizzbuzz.py
-for i in range(1, 15+1):
-    if i % 3 == 0:
+for i in range(6, 30+1):
+     #jbndklfnbld
+    if i % 15 == 0:
+        print('fizzbuzz)
+    elif i % 3 == 0:
         print('fizz')
     elif i % 5 == 0:
         print('buzz')
+
     else:
         print(i)
